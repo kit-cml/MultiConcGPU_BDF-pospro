35024a11c492f02bb50e27d437120daac008aaca
IritaSee <iga.narendra@gmail.com>
2024-09-26 13:35:34 +0900

    add herg flag in shell

--- bin/codetest.sh
+++ bin/codetest.sh
@@ -1,4 +1,4 @@
 cd ..
 make clean all
 cd bin
-./drug_sim -input_deck input_deck_example.txt -hill_file control_me/IC50_drug_control.csv -init_file control_me/init_drug_control.csv
+./drug_sim -input_deck input_deck.txt -hill_file drug/testing/ -herg_file herg/verapamil_2000.csv -init_file control_me/init_drug_control.csv
