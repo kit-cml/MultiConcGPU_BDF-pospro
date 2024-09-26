a8c8fdfb77d39757462c9c4e67304a42c47d60cc
IritaSee <iga.narendra@gmail.com>
2024-09-26 14:40:16 +0900

    add previous result, testing

--- bin/codetest.sh
+++ bin/codetest.sh
@@ -1,4 +1,4 @@
 cd ..
 make clean all
 cd bin
-./drug_sim -input_deck input_deck.txt -hill_file drug/testing/ -herg_file herg/verapamil_2000.csv -init_file control_me/init_drug_control.csv
+./drug_sim -input_deck input_deck_example.txt -hill_file drug/testing/ -herg_file herg/verapamil_2000.csv -init_file init_IC50_verapamil/_state_only.csv
